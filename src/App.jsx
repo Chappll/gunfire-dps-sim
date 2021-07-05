79d30554aeaeb72c33f513e5aa12088a21047347
Chappll <68033975+Chappll@users.noreply.github.com>
2021-07-05 21:32:23 +0100

    Title Change, Css changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import './App.css'
 import React, { useState, useEffect } from 'react'
+// import { Button } from 'react-bootstrap'
 import firebase from './firebase'
 import DpsChart from './Components/DpsChart'
 import OptionsForm from './Components/OptionsForm'
@@ -12,17 +13,25 @@ function App() {
 	const [options, setOptions] = useState({ Crit: false, Reloads: true, WpnLevel: 0, Category: 'All', Health: 'Health', MinROF: false, MinDamage: false })
 	const ref = firebase.firestore().collection('Weapons')
 
-	// const addWeapon = (event) => {
-	//   console.log(ref)
-	//       ref
-	//       .doc("Flowing Light")
-	//       .set(
-	//         ({Category: "Melee", Damage: 490, CritX: 2, Rof: 1.88, Magazine: 10, Reload: 1.45, DmgType: "L", EChance: 0.5,
-	//         PicLink: "https://static.wikia.nocookie.net/gunfire_reborn/images/6/64/FlowingLight.png/revision/latest?cb=20210221023313"})
-	//       )
-	//       .catch((err) => {
-	//         console.error(err);
-	//       });
+	// const addWeapon = () => {
+	// 	console.log(ref)
+	// 	ref
+	// 		.doc('Fire Tower')
+	// 		.set(
+	// 			({ Category: 'Melee',
+	// 				Damage: 750,
+	// 				CritX: 2,
+	// 				Rof: 1.5,
+	// 				Magazine: 1,
+	// 				Reload: 0,
+	// 				DmgType: 'F',
+	// 				EChance: 0.5,
+	// 				AOE: 5,
+	// 				PicLink: 'https://static.wikia.nocookie.net/gunfire_reborn/images/c/c4/Fire_Tower.png' })
+	// 		)
+	// 		.catch((err) => {
+	// 			console.error(err)
+	// 		})
 	// }
 
 	const getWeapons = () => {
@@ -54,7 +63,7 @@ function App() {
 			<img className="logo" src={logo} alt="logo" height={300} width={600} />
 			<OptionsForm optionsUpdate={optionsUpdate} />
 			<header className="App-header">
-				{// <button onClick={addWeapon}>Add</button>
+				{// <Button onClick={addWeapon}>Add</Button>
 				}
 				{!loading && <DpsChart weaponNames={weaponNames} weaponStats={weaponStats} options={options} />}
 			</header>
